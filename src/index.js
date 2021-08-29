6e034c9555ef4c3ad3b4bab9662cb600acf120ae
osipm <54916138+osipm@users.noreply.github.com>
2021-08-29 17:57:24 +0300

    зроблена розмітка карточок

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import './sass/main.scss';
+import './styles.css';
+
+import menu from './menu.json';
+import templateCard from './templates/template-card.hbs';
+
+const cardRef = document.querySelector('.js-menu');
+cardRef.insertAdjacentHTML('beforeend', templateCard(menu));
