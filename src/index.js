82447e0f537b9714a5f7751e6ecd28874bce84a5
osipm <54916138+osipm@users.noreply.github.com>
2021-08-29 21:02:40 +0300

    готова

--- src/index.js
+++ src/index.js
@@ -5,3 +5,26 @@ import templateCard from './templates/template-card.hbs';
 
 const cardRef = document.querySelector('.js-menu');
 cardRef.insertAdjacentHTML('beforeend', templateCard(menu));
+
+const body = document.querySelector('body');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const toggle = document.querySelector('#theme-switch-toggle');
+toggle.addEventListener('change', changeTheme);
+
+toggle.checked = localStorage.theme === Theme.DARK;
+changeTheme();
+
+function changeTheme() {
+  if (toggle.checked) {
+    document.body.className = Theme.DARK;
+    localStorage.theme = Theme.DARK;
+  } else {
+    document.body.className = Theme.LIGHT;
+    localStorage.theme = Theme.LIGHT;
+  }
+}
